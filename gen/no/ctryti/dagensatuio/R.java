665c9a0e4aea72942d49b5754a3442e6b8e97dfd
Daniel Peder Dahlsveen <danieldahlsveen@gmail.com>
2010-03-19 17:14:40 +0100

    Now properly displays the list and icons, and the bottom textfield in the home_activity layout

--- gen/no/ctryti/dagensatuio/R.java
+++ gen/no/ctryti/dagensatuio/R.java
@@ -10,20 +10,35 @@ package no.ctryti.dagensatuio;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int background_hvga=0x7f020000;
+        public static final int colors=0x7f020001;
+        public static final int icon_base=0x7f020002;
+        public static final int icon_day=0x7f020003;
+        public static final int icon_friday=0x7f020004;
+        public static final int icon_monday=0x7f020005;
+        public static final int icon_thursday=0x7f020006;
+        public static final int icon_tuesday=0x7f020007;
+        public static final int icon_wednesday=0x7f020008;
+    }
     public static final class id {
-        public static final int day_list=0x7f040002;
-        public static final int desc=0x7f040001;
-        public static final int dish_list=0x7f040004;
-        public static final int period=0x7f040003;
-        public static final int type=0x7f040000;
+        public static final int day_icon=0x7f060002;
+        public static final int days_list=0x7f060005;
+        public static final int desc=0x7f060001;
+        public static final int dish_list=0x7f060006;
+        public static final int home_bottom=0x7f060004;
+        public static final int period=0x7f060003;
+        public static final int type=0x7f060000;
     }
     public static final class layout {
-        public static final int custom_list_row=0x7f020000;
-        public static final int day_list_row=0x7f020001;
-        public static final int dinner_item_row=0x7f020002;
-        public static final int home_activity=0x7f020003;
+        public static final int custom_list_row=0x7f030000;
+        public static final int day_icon=0x7f030001;
+        public static final int home_activity=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f050000;
+    }
+    public static final class style {
+        public static final int DishTypeTheme=0x7f040000;
     }
 }
